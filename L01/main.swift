a9c4c3212569fde2e84f8f96184292a465b915a3
20161104598 <a20161104598@admindeimac-32.local>
2018-09-12 11:14:41 +0800

    Initial Commit

---
+++ L01/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  L01
+//
+//  Created by 20161104598 on 18/9/12.
+//  Copyright © 2018年 20161104598. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
